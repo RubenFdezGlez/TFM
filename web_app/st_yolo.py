6a612c4fcbedcb58d2593e4950304e5e5a496a22
RubenFdezGlez <rferng19@estudiantes.unileon.es>
2026-06-03 19:15:27 +0200

    feat: code optimization and report generator

--- web_app/st_yolo.py
+++ web_app/st_yolo.py
@@ -3,6 +3,7 @@
 
     camera_input_live: A streamlit component to capture live video feed from the camera.
     cv2: Modifies the image to draw the bounding boxes and the license plate text.
+    datatime: Gets the current time for the reports.
     numpy: Used for image manipulation and conversion between PIL and OpenCV formats.
     PIL: Used for image manipulation and conversion between OpenCV and PIL formats.
     pytesseract: Extracts the text from the detected license plate regions.
@@ -10,10 +11,14 @@
     streamlit: Used for creating the web interface for uploading images and displaying results.
     tempfile: Used to store the video in the video processing option.
     torch: Enables GPU usage to accelerate model inference.
+    threading: Uses multiple threads to process the different camera inputs.
     ultralytics: Provides the loading of the YOLO model, which is used for vehicle and license plate detection.
+    xlsxwriter: Module for creating Excel files and write information like date, time and location into them.
+    zoneInfo: Gets the time information for the local zone (Madrid).
 """
 from camera_input_live import camera_input_live
 import cv2
+from datetime import datetime
 import numpy as np
 from PIL import Image
 import pytesseract
@@ -22,7 +27,10 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import tempfile
 import torch
+import threading
 from ultralytics import YOLO
+import xlsxwriter
+from zoneinfo import ZoneInfo
 
 
 def reset_session_state():
@@ -32,15 +40,31 @@ def reset_session_state():
 
 
 def getTesseractText(cropped_img):
-    """Extract text from the cropped license plate image using the engine Tesseract OCR."""
+    """Extract text from the cropped license plate image using the engine Tesseract OCR. Removes any non alphanumeric characters (such as '&' or '%'), including the space.
+
+    Args:
+        cropped_img (MatLike): Image object from the cv2 module containing only the license plate region.
+    
+    Returns:
+        text (str): The text extracted from the license plate region, containing only digits, uppercase letters and spaces.
+    """
     text = pytesseract.image_to_string(cropped_img, config='--psm 8')
     text = [char for char in text if char.isdigit() or char.isspace() or char.isupper()]
+
     return ''.join(text)
 
 
 @st.cache_resource
 def load_models(device):
-    """Load the vehicle and license plate detection models."""
+    """Load the vehicle and license plate detection models.
+
+    Args:
+        device (torch.device): The device to load the models on, either CPU or GPU.
+
+    Returns:
+        vehicle_model (YOLO): The loaded YOLO model for vehicle detection.
+        lp_model (YOLO): The loaded YOLO model for license plate detection.
+    """
     vehicle_model = YOLO('./models/v_det/best.pt').to(device)
     lp_model = YOLO('./models/lp_det/best.pt').to(device)
 
@@ -49,7 +73,11 @@ def load_models(device):
 
 @st.dialog("Matrícula detectada")
 def lp_detected(lp):
-    """Shows a dialog when a license plate that is on the list of plates to detect is found in the image."""
+    """Shows a dialog when a license plate that is on the list of plates to detect is found in the image.
+
+    Args:
+        lp (str): The license plate that has been detected and is on the list of plates to detect.
+    """
     st.write(f":red[La matrícula {lp} ha sido detectada]")
 
 
@@ -60,6 +88,9 @@ def combineDetections(v_results, lp_results, img):
         v_results (Results): Results object obtained from the vehicle detection on the image/frame.
         lp_results (Results): Results object obtained from the license plate detection on the image/frame.
         img (MatLike): Image object from the cv2 module.
+    
+    Returns:
+        img_copy (MatLike): Image object from the cv2 module with the bounding boxes and license plate text drawn on it.
     """
     img_copy = img
 
@@ -95,7 +126,11 @@ def combineDetections(v_results, lp_results, img):
 
 @st.cache_resource
 def load_lp():
-    """Load the license plates from the file."""
+    """Load the license plates from the file.
+    
+    Returns:
+        lps (list): A list of license plates to be detected, obtained from the file.
+    """
     lps = []
 
     file = open("./web_app/lp.txt", "r")
@@ -108,8 +143,14 @@ def load_lp():
 
 
 def valid_lp(lp):
-    """Check if the license plate has the correct structure"""
+    """Check if the license plate has the correct structure for Spanish license plates.
+    
+    Args:
+        lp (str): The license plate to be checked.
 
+    Returns:
+        valid (bool): True if the license plate has a valid structure, False otherwise.
+    """
     # Matrícula normal de vehículos (también se aplica a taxis y VCTs)
     if re.fullmatch(r'[0-9]{4}[A-Z]{3}', lp):
         return True 
@@ -149,14 +190,14 @@ def lp_list():
     with col1:
         with st.popover("Añadir matrícula"):
             lp = st.text_input("Introduce la matrícula. Sólo se permiten formatos de matrículas españolas y sin espacios.")
-            if st.button("Añadir matrícula"):
+            if st.button("Añadir matrícula", key="addLPToList"):
                 if valid_lp(lp):
                     st.session_state.lps.append(lp) 
                 else:
                     st.write(":red[La matrícula introducida no tiene el formato correcto.]")
                    
     with col2:
-        save_lp = st.button("Guardar")
+        save_lp = st.button("Guardar", key="saveLPList")
     
     if save_lp:
         file = open("./web_app/lp.txt", "w")
@@ -174,7 +215,69 @@ def lp_list():
             with cont_col2:
                 if st.button("Remove", key=str(idx)+lp):
                     st.session_state.lps.pop(idx)
-        
+
+
+def generate_report(cam_number, location):
+    report = xlsxwriter.Workbook(f'report_camera_{cam_number}.xlsx')
+    rep_sheet = report.add_worksheet()
+
+    dt = datetime.now().astimezone(ZoneInfo('Europe/Madrid'))
+
+    labels = ["Date", "Time", "Location"]
+    data = [dt.strftime("%d/%m/%Y"), dt.strftime("%X"), location]
+
+    for column, (label, dat) in enumerate((labels, data)):
+        rep_sheet.write(0, column, label)
+        rep_sheet.write(1, column, dat)
+
+    report.close()
+
+
+def changePredictionState(index):
+    st.session_state.predictions_state[index] = "Activar predicción" if st.session_state.predictions_state[index] == "Parar predicción" else "Parar predicción"
+
+
+def predict_camera_live(camera_num, placeholder):
+    while cap.isOpened():
+
+            ret, frame = cap.read() if cap.isOpened() else (False, None)
+            
+            if ret and frame is not None:
+
+                if st.session_state.predictions_state[camera_num] == "Parar predicción":
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                    v_results = vehicle_model.predict(frame, device=device, conf=v_conf_threshold)
+                    lp_results = lp_model.predict(frame, device=device, conf=lp_conf_threshold)
+                    img_detected = combineDetections(v_results, lp_results, frame)
+
+                    placeholder.image(img_detected, caption=f"Cámara {camera_num}", width="stretch")
+                else:
+                    placeholder.image(frame, caption=f"Cámara {camera_num}", width="stretch")
+
+
+def createMultiCamSection(camera_num, coords):
+    placeholder = st.empty()
+    col1, col2 = st.columns(2)
+    with col1:
+        report = st.button(f"Informe cámara {camera_num}", key=f"rep{camera_num}")
+    with col2:
+        pred = st.button(st.session_state.predictions_state[camera_num], key=f"inf{camera_num}")
+
+    if report:
+        generate_report(f"{camera_num}", coords[camera_num])
+    if pred:
+        changePredictionState(camera_num)
+    
+    cap = cv2.VideoCapture(camera_num)
+
+    if not cap.isOpened():
+        st.warning(f"No se pudo abrir la cámara {camera_num}")   
+
+    predict_camera_live(camera_num, placeholder)
+
+    return placeholder, report, pred
+
 
 # Import the models and the license plate list and load Tesseract OCR to be used on the detections.
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -186,18 +289,21 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 st.set_page_config(layout="wide", page_title="Detección de Vehículos", page_icon="🚗")
 st.title("Detección de vehículos y matrículas")
 
+# State variable for application option between: "Archivo", "Camara", "Video" and "Multi-cam".
 if 'modo_entrada' not in st.session_state:
     st.session_state.modo_entrada = "Archivo"
+# State variable for options "Archivo" and "Camara" to show the last image/frame on the screen.
 if 'imagen_actual' not in st.session_state:
     st.session_state.imagen_actual = None
+# State variable for option "Video" to show the last video on the screen.
 if 'video_actual' not in st.session_state:
     st.session_state.video_actual = None 
+# State variable for option "Video" to start or pause the video with the buttons.
 if 'video_processing' not in st.session_state:
     st.session_state.video_processing = False
-if 'current_frame' not in st.session_state:
-    st.session_state.current_frame = None
-if 'stop_video' not in st.session_state:
-    st.session_state.stop_video = False
+# State variables for option "Multi-cam" for changing the buttons values to start or stop doing predictions.
+if 'predictions_state' not in st.session_state:
+    st.session_state.predictions_state = ["Activar predicción","Activar predicción","Activar predicción","Activar predicción"]
 
 
 with st.sidebar:
@@ -205,18 +311,18 @@ with st.sidebar:
 
     entrada = st.radio("Fuente de imagen", 
                        ("Archivo", "Camara", "Video", "Multi-cam"),
-                       index=0 if st.session_state.modo_entrada == "Archivo" else 1 if st.session_state.modo_entrada == "Camara" else 2 if st.session_state.modo_entrada == "Multi-cam" else 3,
+                       index=0 if st.session_state.modo_entrada == "Archivo" else 1 if st.session_state.modo_entrada == "Camara" else 2 if st.session_state.modo_entrada == "Video" else 3,
                        on_change=reset_session_state,
                        horizontal=True)
 
-    st.session_state.modo_entrada = "Archivo" if entrada == "Archivo" else "Camara" if entrada == "Camara" else "Video"\
-          if entrada == "Video" else "Multi-cam" 
+    st.session_state.modo_entrada = "Archivo" if entrada == "Archivo" else "Camara" if entrada == "Camara" else "Video" if entrada == "Video" else "Multi-cam" 
+    
 
     st.divider()
 
     if "lp_list" not in st.session_state:
         st.write("Lista de matrículas")
-        if st.button("Cambiar"):
+        if st.button("Cambiar", key="changeLpList"):
             lp_list()
 
     st.divider()
@@ -239,12 +345,13 @@ with st.sidebar:
 
 
 with st.container():
+
     # Option for single image detection
     if st.session_state.modo_entrada == "Archivo":
         uploaded_file = st.file_uploader("Sube una imagen", 
                                          type=["jpg", "jpeg", "png"],
                                          accept_multiple_files=False,
-                                         key="file_uploader")
+                                         key="img_uploader")
         
         if uploaded_file is not None:
             img = Image.open(uploaded_file).convert("RGB")
@@ -256,6 +363,7 @@ with st.container():
 
                 img_copy = combineDetections(v_results, lp_results, img)
                 st.session_state.imagen_actual = img_copy
+
     # Option for live camera feed
     elif st.session_state.modo_entrada == "Camara":
         st.info("Mostrando la cámara en vivo. Asegúrate de que tu cámara esté conectada y funcionando.")
@@ -273,16 +381,17 @@ with st.container():
                     st.session_state.imagen_actual = img_detected
             else:
                 st.error("No se pudo acceder a la cámara. Por favor, verifica tu conexión y permisos.")
+
     # Option for single video files
     elif st.session_state.modo_entrada == "Video":
-        uploaded_file = st.file_uploader("Sube una imagen", 
+        uploaded_file = st.file_uploader("Sube un vídeo", 
                                          type=['mp4', 'avi', 'mov', 'mkv', 'webm'],
                                          accept_multiple_files=False,
-                                         key="file_uploader")
+                                         key="video_uploader")
         
         col1, col2 = st.columns(2)
         with col1:
-            start_button = st.button("▶️ Iniciar", type="primary", key="start_button")
+            start_button = st.button("▶️ Iniciar", key="start_button")
         with col2:
             stop_button = st.button("⏹️ Detener", key="stop_button")
 
@@ -293,15 +402,13 @@ with st.container():
             st.session_state.video_file_path = tfile.name
             st.session_state.last_uploaded_name = uploaded_file.name
             st.session_state.video_processing = False
-            st.session_state.current_frame = None
 
-        if start_button:
+        if start_button and uploaded_file is not None:
             st.session_state.video_processing = True
-            st.session_state.stop_video = False
         
         if stop_button:
             st.session_state.video_processing = False
-            st.session_state.stop_video = True
+            print(st.session_state.modo_entrada)
  
         # Open the video capture on the file, read each frame and do detection on each one
         if st.session_state.video_processing:
@@ -324,85 +431,23 @@ with st.container():
                     detected_frame = combineDetections(v_results, lp_results, img)
                     video_placeholder.image(detected_frame, channels="BGR", width='stretch')
                     
-                    # Check if stop was requested
-                    if st.session_state.get('stop_video', False):
-                        st.session_state.video_processing = False
-                        break
                 cap.release()
-    else:
+
+    # Option for multiple inputs or cameras
+    elif st.session_state.modo_entrada == "Multi-cam":
+        print(st.session_state.modo_entrada)
         st.info("Mostrando la función multi-cámara. Asegúrate de que las cámaras estén conectadas y funcionando.")
         with st.spinner("Accediendo a las cámaras"):
+            coords = ["42.588778, -5.576272", "42.596464, -5.577549", "42.588778, -5.576272", "42.596464, -5.577549"]
             col1, col2 = st.columns(2)
 
             with col1:
-                placeholder1 = st.empty()
-                bt1 = st.button("Informe cámara 1")
-                placeholder3 = st.empty()
-                bt2 = st.button("Informe cámara 3")
+                for i in range(0, 3, 2):
+                    createMultiCamSection(i, coords[i])
 
             with col2:
-                placeholder2 = st.empty()
-                bt2 = st.button("Informe cámara 2")
-                placeholder4 = st.empty()
-                bt4 = st.button("Informe cámara 4")
-
-            # Inicializar capturas con diferentes índices
-            cap1 = cv2.VideoCapture(0)
-            cap2 = cv2.VideoCapture(1)
-            cap3 = cv2.VideoCapture(2)
-            cap4 = cv2.VideoCapture(3)
-
-            if not cap1.isOpened():
-                st.error("No se pudo abrir la cámara 1")
-            if not cap2.isOpened():
-                st.warning("No se pudo abrir la cámara 2")
-            if not cap3.isOpened():
-                st.warning("No se pudo abrir la cámara 3")
-            if not cap4.isOpened():
-                st.warning("No se pudo abrir la cámara 4")
-
-            while cap1.isOpened() or cap2.isOpened() or cap3.isOpened() or cap4.isOpened():
-                # Leer frames
-                ret1, frame1 = cap1.read() if cap1.isOpened() else (False, None)
-                ret2, frame2 = cap2.read() if cap2.isOpened() else (False, None)
-                ret3, frame3 = cap3.read() if cap3.isOpened() else (False, None)
-                ret4, frame4 = cap4.read() if cap4.isOpened() else (False, None)
-                
-                if ret1 and frame1 is not None:
-                    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
-
-                    v_results = vehicle_model.predict(frame1, device=device, conf=v_conf_threshold)
-                    lp_results = lp_model.predict(frame1, device=device, conf=lp_conf_threshold)
-                    img_detected = combineDetections(v_results, lp_results, frame1)
-
-                    placeholder1.image(frame1, caption="Cámara 1", use_container_width=True)
-                
-                if ret2 and frame2 is not None:
-                    frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
-
-                    v_results = vehicle_model.predict(frame2, device=device, conf=v_conf_threshold)
-                    lp_results = lp_model.predict(frame2, device=device, conf=lp_conf_threshold)
-                    img_detected = combineDetections(v_results, lp_results, frame2)
-
-                    placeholder2.image(img_detected, caption="Cámara 2", use_container_width=True)
-
-                if ret3 and frame3 is not None:
-                    frame3 = cv2.cvtColor(frame3, cv2.COLOR_BGR2RGB)
-
-                    v_results = vehicle_model.predict(frame3, device=device, conf=v_conf_threshold)
-                    lp_results = lp_model.predict(frame3, device=device, conf=lp_conf_threshold)
-                    img_detected = combineDetections(v_results, lp_results, frame3)
-
-                    placeholder2.image(img_detected, caption="Cámara 3", use_container_width=True)
-
-                if ret4 and frame4 is not None:
-                    frame4 = cv2.cvtColor(frame4, cv2.COLOR_BGR2RGB)
-
-                    v_results = vehicle_model.predict(frame4, device=device, conf=v_conf_threshold)
-                    lp_results = lp_model.predict(frame4, device=device, conf=lp_conf_threshold)
-                    img_detected = combineDetections(v_results, lp_results, frame4)
-
-                    placeholder2.image(img_detected, caption="Cámara 2", use_container_width=True)
+                for i in range(1, 3, 2):
+                    createMultiCamSection(i, coords[i])
 
 
 # Shows the detections only on the single image option
