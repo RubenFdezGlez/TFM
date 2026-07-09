9c6841a4bf0e1bd5b571791a58799ff80f9a8e08
RubenFdezGlez <rferng19@estudiantes.unileon.es>
2026-07-09 16:09:22 +0200

    fix: lp validation for testing

--- lp_scripts/yolo_lp.py
+++ lp_scripts/yolo_lp.py
@@ -4,6 +4,7 @@
     torch: Enabling GPU usage to accelerate model training and inference.
     ultralytics: Provides the implementation, training and validation of the YOLO model, which is used for vehicle detection.
 """
+import os
 import torch
 from ultralytics import YOLO
 
@@ -26,8 +27,8 @@ if __name__ == '__main__':
     torch.cuda.empty_cache()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    license_plate_det_model = getYOLOModel(device)
-    results_license_plate_det = license_plate_det_model.train(data = "./datasets/UC3M-LP/yolo.yaml",
+    model = getYOLOModel(device)
+    results_license_plate_det = model.train(data = "./datasets/UC3M-LP/yolo.yaml",
                               epochs = 25,
                               patience = 5,
                               batch = 12,
@@ -46,3 +47,20 @@ if __name__ == '__main__':
                               freeze = 0,                            
                               plots = True,
     )
+
+    best_model = YOLO("./runs/detect/y26_lp/weights/best.pt").to(device)
+    for ccpd_subdataset in os.listdir("./datasets/ccpd_yolo"):
+        if ccpd_subdataset.endswith(".yaml"):
+            continue
+        yaml_name = "".join(["./datasets/ccpd_yolo/", ccpd_subdataset, ".yaml"])
+        
+        results = best_model.val(data = yaml_name, batch = 12, workers=4, split="test", device = device, conf = 0.7, iou = 0.7, imgsz = 640, verbose=False)
+
+        # Compute metrics
+        print(f"Results for CCPD_{ccpd_subdataset}")
+        print(f"mAP@0.5: {results.box.map50}")
+        print(f"mAP@0.5:0.95: {results.box.map}")
+        print(f"Precision: {results.box.p}")
+        print(f"Recall: {results.box.r}")
+        print(f"F1-Score: {results.box.f1}")
+        break
