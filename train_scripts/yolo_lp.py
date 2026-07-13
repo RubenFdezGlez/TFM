5fd3defd52ed11c0c56d84541c964fb2c0c54c16
RubenFdezGlez <rferng19@estudiantes.unileon.es>
2026-07-13 18:51:16 +0200

    feat: ccpd training, validation and testing II

---
+++ train_scripts/yolo_lp.py
@@ -0,0 +1,55 @@
+"""
+    Package imports:
+
+    torch: Enabling GPU usage to accelerate model training and inference.
+    ultralytics: Provides the implementation, training and validation of the YOLO model, which is used for vehicle detection.
+"""
+import torch
+from ultralytics import YOLO
+
+
+def getYOLOModel(device):
+    """Load the YOLO model and return it ready for training or inference.
+    
+    Args:
+        device (torch.device): The device (CPU or GPU) to which the model will be sent.
+
+    Returns:
+        YOLO: The loaded YOLO model ready for training or inference.
+    """
+    return YOLO("yolo26n").to(device)     
+
+
+if __name__ == '__main__':
+    
+    """Train the YOLO model for license plate detection."""
+    torch.cuda.empty_cache()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = getYOLOModel(device)
+    model.train(data = "./datasets/UC3M-LP/yolo.yaml", epochs = 15, patience = 3, batch = 12, cache = True, device = device,
+                workers = 4, name = "y26_lp", exist_ok = True, optimizer = "auto", verbose = False, multi_scale = 0.25,
+                cos_lr = False,                    
+    )
+
+    """Fine tune the model using a subdataset of CCPD where the license plates are not very visible."""
+    best_model = YOLO("./runs/detect/y26_lp/weights/best.pt").to(device)
+    best_model.train(data = "./datasets/ccpd_yolo/ccpd_finetuning.yaml", epochs = 5, batch = 12, cache = True, device = device,
+                     workers = 4, name = "y26_lp", exist_ok = True, optimizer = "auto", verbose = False, 
+                     multi_scale = 0.25, cos_lr = False,                    
+    )    
+    
+    """Test the model in the subdataset of CCPD selected"""
+    ccpd_subset = ["ccpd_base", "ccpd_db", "ccpd_fn", "ccpd_rotate", "ccpd_tilt", "ccpd_weather"]
+    yaml_name = "".join(["./datasets/ccpd_yolo/", ccpd_subset[0], ".yaml"])
+    
+    results = best_model.val(data = yaml_name, batch = 12, workers=4, split="test", device = device, 
+                             conf = 0.7, iou = 0.7, imgsz = 640, verbose=False)
+
+    """Compute metrics"""
+    print(f"Results for: {ccpd_subset}")
+    print(f"mAP@0.5: {results.box.map50}")
+    print(f"mAP@0.5:0.95: {results.box.map}")
+    print(f"Precision: {results.box.p}")
+    print(f"Recall: {results.box.r}")
+    print(f"F1-Score: {results.box.f1}")
